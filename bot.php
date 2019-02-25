9f0ac77e00fc65fe505c918c24a174629ccbfe90
BoomsZaa <boomspass@hotmail.com>
2019-02-25 16:48:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -30,6 +30,8 @@ if (!is_null($events['events'])) {
 			$Topic = "CH1" ;
 			if ($text == "on"){
 				$text = "1";
+				sleep(6);
+				$text = "0";
 			}else if($text == "off"){
 				$text = "0";
 			}
@@ -39,9 +41,9 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-sleep(6);
-$Topic = "CH1" ;
-$text = "Off";
+//sleep(6);
+//$Topic = "CH1" ;
+//$text = "0";
 getMqttfromlineMsg($Topic,$text);
 echo "OK3";
 ?>
