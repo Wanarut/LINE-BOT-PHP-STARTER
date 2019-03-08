3bccd6f7402978f884e344086813fe0cdbe6ee86
Wanarut Boonyung <wanarut.b@gmail.com>
2019-03-08 13:22:05 +0700

    test bot.php

--- bot.php
+++ bot.php
@@ -1,31 +1,48 @@
 <?php
-	$access_token = 'rikK8KJjNTEw2AoogxaWeRDJwq0ii5KLMfP4axQnCpp+AZns7rkU7bUT1mtI2rC6SUvAxuoICfjPSEt5EtCJln+vnAwAuoBxE2s6dW6yQyitVVveCH4Yq/clkUec1Rs4+nYEVry4V2UzH9oeTWUMDQdB04t89/1O/w1cDnyilFU=';		// Get POST body 
-	content$content = file_get_contents('php://input');	// Parse JSON
-	$events = json_decode($content, true);				// Validate parsed JSON data
-	if (!is_null($events['events'])) {					// Loop through each event	
-		foreach ($events['events'] as $event) {			// Reply only when message sent is in 'text' format		
-			if ($event['type'] == 'message' && $event['message']['type'] == 'text') {	// Get text sent			
-				$text = $event['message']['text'];			// Get replyToken			
-				$replyToken = $event['replyToken'];			// Build message to reply back			
-				$messages = [
-					'type' => 'text',
-					'text' => $text
-				];			// Make a POST Request to Messaging API to reply to sender			
-				$url = 'https://api.line.me/v2/bot/message/reply';			
-				$data = [
-					'replyToken' => $replyToken,
-					'messages' => [$messages],
-				];			$post = json_encode($data);
-				$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
-				$ch = curl_init($url);			
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");			
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $post);			
-				curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);			
-				$result = curl_exec($ch);			
-				curl_close($ch);			
-				echo $result . "";		
-			}	
+ require("pub.php");
+ require("line.php");
+
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['ESP'])) {
+	
+	send_LINE($events['ESP']);
+		
+	echo "OK";
+	}
+if (!is_null($events['events'])) {
+	echo "line bot";
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+
+			$Topic = "CH1" ;
+			if ($text == "on"){
+				$text = "1";
+			}
+			if($text == "off"){
+				$text = "0";
+			}
+			getMqttfromlineMsg($Topic,$text);
+			   
+			
 		}
-	}echo "OK";
+	}
+}
+sleep(6);
+$Topic = "CH1" ;
+$text = "0";
+getMqttfromlineMsg($Topic,$text);
+echo "OK3";
+?>
